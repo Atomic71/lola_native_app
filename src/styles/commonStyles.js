8b9964698d1bbdefeb05d41cf2c523aa003e3b09
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-21 22:32:37 +0200

    chore: remove flex 1

---
+++ src/styles/commonStyles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  spreadContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});
