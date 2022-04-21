2a02dfe36a5ba8bd55efe42e058557ee75126f82
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-21 22:32:14 +0200

    chore: declare categories as constant

---
+++ src/constants/categories.js
@@ -0,0 +1,30 @@
+import Hrabra from '../assets/categoriesIllustrations/hrabra.svg';
+import Kona from '../assets/categoriesIllustrations/kona.svg';
+import Prakticna from '../assets/categoriesIllustrations/prakticna.svg';
+import Radoznala from '../assets/categoriesIllustrations/radoznala.svg';
+import Zabavna from '../assets/categoriesIllustrations/zabavna.svg';
+
+const categories = [
+  {
+    image: Prakticna,
+    key: 'Prakticna',
+  },
+  {
+    image: Hrabra,
+    key: 'Hrabra',
+  },
+  {
+    image: Zabavna,
+    key: 'Zabavna',
+  },
+  {
+    image: Radoznala,
+    key: 'Radoznala',
+  },
+  {
+    image: Kona,
+    key: 'Kona',
+  },
+];
+
+export default categories;
