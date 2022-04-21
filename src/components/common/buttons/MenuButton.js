6ebf6daa6ca84da66075ac45e142624437fc338b
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-21 21:43:55 +0200

    Lola-5: add buttons for headers

---
+++ src/components/common/buttons/MenuButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+import Menu from '../../../assets/menu.svg';
+
+const MenuButton = () => (
+  <TouchableOpacity>
+    <Menu />
+  </TouchableOpacity>
+);
+
+export default MenuButton;
---
+++ src/components/common/buttons/SearchButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+import Search from '../../../assets/search.svg';
+
+const SearchButton = () => (
+  <TouchableOpacity>
+    <Search />
+  </TouchableOpacity>
+);
+
+export default SearchButton;
