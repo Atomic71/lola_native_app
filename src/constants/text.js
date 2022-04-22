97ca15c661b288502fca90466c8b8d60eaf8d215
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-22 22:12:53 +0200

    feat: add text constants

---
+++ src/constants/text.js
@@ -0,0 +1,18 @@
+export const COPYRIGHT = '© Copyright 2021 Lola Marazin. All rights reserved.';
+export const MENU = {
+  title: 'MENI',
+  navItems: [
+    {text: 'POCETNA', path: ''},
+    {text: 'O LOLI', path: ''},
+    {text: 'AUTORI', path: ''},
+    {text: 'USLOVI KORISTENJA', path: ''},
+  ],
+};
+export const TEXT_SIZE = {
+  title: 'VELICINA PISMA: ',
+  options: [{}],
+};
+export const LAT_CYR = {
+  title: 'PISMO: ',
+  options: [{}],
+};
