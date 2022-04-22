0435833ec45c43da9b76b1547ce4b0bc8107dc64
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-22 22:21:41 +0200

    remove unused vars

--- src/components/menu/MenuModal.js
+++ src/components/menu/MenuModal.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
 import {MENU} from '../../constants/text';
 import ModalWrapper from '../common/ModalWrapper';
 import MenuFooter from './MenuFooter';
 import MenuNavItems from './MenuNavItems';
-
-const styles = StyleSheet.create({
-  container: {
-    paddingLeft: 10,
-    paddingRight: 20,
-  },
-  mainNav: {
-    marginBottom: 20,
-  },
-  footerWrapper: {
-    paddingTop: 18,
-  },
-});
 
 const MenuModal = ({navigation}) => {
   return (
