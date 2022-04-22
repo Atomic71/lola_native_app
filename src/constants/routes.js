801c7d5c31a78d58a100516fd1dc59aca8b85987
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-22 23:52:45 +0200

    add about view

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,6 @@
 export const APP_ROUTES = {
   terms: 'tac',
   home: 'home',
+  about: 'about',
+  authors: 'authors',
 };
---
+++ src/views/AboutView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import commonStyles from '../styles/commonStyles';
+
+const AboutView = () => {
+  return (
+    <View style={[commonStyles.horizontalPadding, commonStyles.commonScreen]}>
+      {/* <ScrollView style={{}}></ScrollView> */}
+    </View>
+  );
+};
+
+export default AboutView;
