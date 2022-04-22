5b0cabdd19946ff1e9570f2a749e1f0d99da4f00
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-22 15:21:53 +0200

    chore: move home view to own file

---
+++ src/views/HomeView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+
+const HomeView = () => {
+  return (
+    <ScrollView>
+      <Text>Hello world</Text>
+    </ScrollView>
+  );
+};
+
+export default HomeView;
