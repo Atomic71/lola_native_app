6bfea8798bda7715b14af0aff00ea5d6e6322f04
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-22 15:22:02 +0200

    chore: create modal header

---
+++ src/components/common/ModalHeader.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import commonStyles from '../../styles/commonStyles';
+import CloseButton from './buttons/CloseButton';
+
+const styles = StyleSheet.create({
+  modalHeader: {
+    backgroundColor: 'white',
+  },
+});
+const ModalHeader = props => {
+  return (
+    <SafeAreaView
+      style={[commonStyles.horizontalPadding, styles.modalHeader]}
+      edges={['left', 'right', 'top']}>
+      <CloseButton onPress={() => props.navigation.goBack()} />
+    </SafeAreaView>
+  );
+};
+
+export default ModalHeader;
