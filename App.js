8e7dd9a47f951051eb6a78efcd9ac2a9b2120996
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-22 15:23:10 +0200

    chore: add RootNavigation

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React, {useEffect} from 'react';
-import {ScrollView, Text} from 'react-native';
-import SplashScreen from 'react-native-splash-screen';
-import {useWindowDimensions} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
+import RootNavigation from './src/navigation/RootNavigation';
 
-import LolaHeader from './src/components/LolaHeader';
-const Stack = createNativeStackNavigator();
-const HomeComponent = () => {
-  return (
-    <ScrollView>
-      <Text>Hello world</Text>
-    </ScrollView>
-  );
-};
 const App = () => {
-  const {width} = useWindowDimensions();
   useEffect(() => {
     setTimeout(() => {
       SplashScreen.hide();
@@ -24,23 +11,7 @@ const App = () => {
   }, []);
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="home"
-          defaultScreenOptions={{
-            headerStyle: {
-              width,
-            },
-          }}>
-          <Stack.Screen
-            name="home"
-            component={HomeComponent}
-            options={{
-              header: LolaHeader,
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <RootNavigation />
     </SafeAreaProvider>
   );
 };
