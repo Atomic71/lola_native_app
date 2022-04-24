0b900091c147ec10e545c04e40a7c6b42cb116ed
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-24 02:25:09 +0200

    feat: implement posts in home view

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -2,14 +2,15 @@ import React from 'react';
 import {View} from 'react-native';
 import LolaCategories from '../components/LolaCategories';
 import PostList, {mockPosts} from '../components/posts/PostList';
+import PostTrack from '../components/posts/PostTrack';
 import commonStyles from '../styles/commonStyles';
 
 const HomeView = () => {
   return (
     <View style={[commonStyles.horizontalPadding, commonStyles.commonScreen]}>
-      <LolaCategories />
-      <PostList posts={mockPosts} />
-      {/* <ScrollView style={{}}></ScrollView> */}
+      <LolaCategories wrapperStyles={[commonStyles.gutter]} />
+      <PostTrack posts={mockPosts} wrapperStyles={[commonStyles.gutter]} />
+      <PostList posts={mockPosts} wrapperStyles={[commonStyles.gutter]} />
     </View>
   );
 };
