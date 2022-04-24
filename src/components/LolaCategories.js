4147e9d51b0d21ddee8b952650a8838def3230b2
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-25 01:35:13 +0200

    feat: Add navigation for svg imgs

--- src/components/LolaCategories.js
+++ src/components/LolaCategories.js
@@ -1,15 +1,20 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {View, TouchableOpacity} from 'react-native';
 import categories from '../constants/categories';
+import {APP_ROUTES} from '../constants/routes';
 
 import commonStyles from '../styles/commonStyles';
 
 const LolaCategories = ({wrapperStyles = []}) => {
+  const {navigate} = useNavigation();
   return (
     <View style={[commonStyles.spreadContainer, ...wrapperStyles]}>
       {categories.map(({image: SvgImg, key}) => {
         return (
-          <TouchableOpacity key={key}>
+          <TouchableOpacity
+            key={key}
+            onPress={() => navigate(APP_ROUTES.category, {category: key})}>
             <SvgImg />
           </TouchableOpacity>
         );
