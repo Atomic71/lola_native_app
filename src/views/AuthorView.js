4a029021f8a38af542c24d45fc1e3952e56bfe28
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-25 01:37:11 +0200

    feat: finish AuthorView

--- src/views/AuthorView.js
+++ src/views/AuthorView.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import AuthorProfile from '../components/authors/AuthorProfile';
 import {BackButton} from '../components/common/Buttons';
 import {Body} from '../components/common/Typography';
 import PostList, {mockPosts} from '../components/posts/PostList';
-import {AUTHOR, COMMON} from '../constants/text';
+import {COMMON} from '../constants/text';
 import useAuthor from '../hooks/useAuthor';
 import commonStyles from '../styles/commonStyles';
 
@@ -13,6 +13,7 @@ const styles = StyleSheet.create({
   viewContainer: {
     position: 'relative',
     paddingTop: 20,
+    flex: 1,
   },
   backButtonContainer: {
     position: 'absolute',
@@ -25,28 +26,25 @@ const AuthorView = ({navigation, goBack, route: {params}, ...rest}) => {
   return (
     <SafeAreaView
       style={[commonStyles.horizontalPadding, styles.viewContainer]}>
-      <View style={[]}>
+      <View>
         <BackButton
           style={[styles.backButtonContainer]}
           onPress={navigation.goBack}
         />
-        {loadingAuthor ? (
-          <Body content={COMMON.loading} />
-        ) : (
-          <React.Fragment>
-            <AuthorProfile
-              author={author}
-              wrapperStyles={[commonStyles.gutter]}
-            />
-
-            <PostList posts={mockPosts} hideAuthorInfo title={AUTHOR.posts} />
-          </React.Fragment>
-        )}
       </View>
-      {/* <ScrollView
-        style={[commonStyles.horizontalPaddingTiny, {backgroundColor: 'red'}]}>
-        <Text>AuthorView</Text>
-      </ScrollView> */}
+      {loadingAuthor ? (
+        <Body content={COMMON.loading} />
+      ) : (
+        <React.Fragment>
+          <AuthorProfile
+            author={author}
+            wrapperStyles={[commonStyles.gutter]}
+          />
+        </React.Fragment>
+      )}
+      <ScrollView contentContainerStyle={{}}>
+        <PostList posts={mockPosts} hideAuthorInfo title={COMMON.posts} />
+      </ScrollView>
     </SafeAreaView>
   );
 };
