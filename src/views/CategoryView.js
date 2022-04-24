abdd70433a187084c3b1c2b3dbf2b5baa4953938
Andrej Tomic <tomicandrej71@gmail.com>
2022-04-25 01:34:53 +0200

    feat: finish category view

--- src/views/CategoryView.js
+++ src/views/CategoryView.js
@@ -1,11 +1,19 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {ScrollView} from 'react-native';
+import PostList, {mockPosts} from '../components/posts/PostList';
+import categories from '../constants/categories';
+import {COMMON} from '../constants/text';
+import commonStyles from '../styles/commonStyles';
 
-const CategoryView = () => {
+const CategoryView = ({route}) => {
+  const {category} = route.params;
+  const categoryDict = categories.find(({key}) => key === category);
+  console.log(categoryDict);
   return (
-    <View>
-      <Text>CategoryView</Text>
-    </View>
+    <ScrollView
+      style={[commonStyles.commonScreen, commonStyles.horizontalPadding]}>
+      <PostList posts={mockPosts} title={COMMON.posts} />
+    </ScrollView>
   );
 };
 
